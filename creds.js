b09c5635e62d7d2e2597ca87abf2c8f6811baab5
Jorge Zamudio <jorzarios@users.noreply.github.com>
2017-11-30 17:51:03 -0600

    Add server.js
    adding server.js for deploy

---
+++ creds.js
@@ -0,0 +1,7 @@
+'use strict'
+const creds = {
+  user: 'jjj172295@gmail.com',
+  pass: 'jjj172295'
+}
+
+module.exports = creds
---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express')
+var app = express()
+var nodemailer = require('nodemailer')
+var creds = require('./creds');
+
+var transporter = nodemailer.createTransport({
+    service : 'gmail',
+    host: "smtp.gmail-com",
+    auth: {
+      user: creds.user,
+      pass: creds.pass
+    }
+  })
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+ res.sendFile(__dirname+'/views/index.html');
+});
+
+app.get('/about', function (req, res) {
+ res.sendFile(__dirname+'/views/about.html');
+});
+app.get('/portafolio', function (req, res) {
+ res.sendFile(__dirname+'/views/portafolio.html');
+});
+app.get('/contacto', function (req, res) {
+ res.sendFile(__dirname+'/views/contacto.html');
+});
+
+app.get('/send', function (req,res) {
+
+ var mailOptions = {
+   from: 'Jorzarios.co',
+   to: 'jorzarios@gmail.com',
+   subject: req.query.subject,
+   text: `Datos del cliente: ${req.query.name}, ${req.query.email}, ${req.query.phone}`
+ }
+ console.log(mailOptions);
+
+ transporter.sendMail(mailOptions, function (error, response) {
+   if(error){
+     console.log(error);
+     res.end('error');
+   }else {
+     console.log("Message sent ");
+     res.end('sent')
+   }
+ })
+})
+
+app.listen(3000, function (err) {
+  if(err) return console.log('Hubo un error'), process.exit(1);
+
+  console.log('Escuchando en el puerto 3000');
+})
