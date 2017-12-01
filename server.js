d41d699c15e708116593ac4930c398224691e9f3
Jorge Zamudio <jorzarios@users.noreply.github.com>
2017-12-01 12:53:19 -0600

    Jorzarios

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/send', function (req,res) {
  })
 })
 
-app.listen(3000, function (err) {
+app.listen(80, function (err) {
   if(err) return console.log('Hubo un error'), process.exit(1);
 
   console.log('Escuchando en el puerto 3000');
